f5174f086591ef93112acd8184965d0cba3f115a
andlomeli <andrea.p.lomeli@gmail.com>
2026-05-07 14:56:09 -0600

    Added new test case scenario

---
+++ TestProject1/TestBasics.cs
@@ -0,0 +1,104 @@
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System.Collections.ObjectModel;
+
+namespace TestProject1;
+
+[TestFixture]
+public class TestBasics
+{
+    protected IWebDriver driver;
+    protected static ExtentReports Reports;
+    protected static ExtentTest Test;
+    protected static string reportPath;
+
+    //Ignores ads because fuck em
+    protected void JsClick(IWebElement element)
+    {
+        ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].click();", element);
+    }
+
+    [OneTimeSetUp]
+    public void SingleUse()
+    {
+        //ToDo: look into how to check if a file is present, check to see the version, and then increment the version file name by 1.
+        //Creates report and determines its location
+        reportPath = Path.Combine(System.Environment.ExpandEnvironmentVariables("%userprofile%/downloads/"), "TestResults.html");
+        var reporter = new ExtentSparkReporter(reportPath);
+        Reports = new ExtentReports();
+        Reports.AttachReporter(reporter);
+    }
+
+    [OneTimeTearDown]
+    public void SingleUseTear()
+    {
+        Reports.Flush();
+        System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+        {
+            FileName = reportPath,
+            UseShellExecute = true
+        });
+    }
+
+    [SetUp]
+    public void Setup()
+    {
+        //Runs before EACH test
+        driver = new FirefoxDriver();
+
+        //Creates the object which will keep track of test results
+        Test = Reports.CreateTest(TestContext.CurrentContext.Test.Name);
+    }
+
+    [TearDown]
+    public void Teardown()
+    {
+        //Retrieves test status from run - if failed takes a screenshot.
+        //If the test passes the status will be updated alone
+        var status = TestContext.CurrentContext.Result.Outcome.Status;
+        var message = TestContext.CurrentContext.Result.Message;
+
+        if (status == NUnit.Framework.Interfaces.TestStatus.Failed)
+        {
+            //Log the actual failure reason fron Nunit before taking the screenshot
+            Test.Log(Status.Fail, $"Assertion failed: {message}");
+
+            var screenshot = ((ITakesScreenshot)driver).GetScreenshot();
+            Test.Fail("Test failed — see screenshot")
+                .AddScreenCaptureFromBase64String(screenshot.AsBase64EncodedString);
+        }
+        else
+        {
+            Test.Pass("Test passed");
+        }
+
+        //Runs after each test
+        //Disposes of previous cache before a new test is run
+        driver?.Quit();
+        driver?.Dispose();
+        driver = null;
+
+    }
+
+    protected IWebElement findElementByCSS(string selector)
+    {
+        IWebElement foundCSSElement = driver.FindElement(By.CssSelector(selector));
+        return foundCSSElement;
+    }
+
+    protected IWebElement findElementByXPath(string xpath)
+    {
+        IWebElement foundXPathElement = driver.FindElement(By.XPath(xpath));
+        return foundXPathElement;
+    }
+
+    protected ReadOnlyCollection < IWebElement> findElementSByXPath(string xPathMulti)
+    {
+        var foundMultipleEle = driver.FindElements(By.XPath(xPathMulti));
+        return foundMultipleEle;
+
+    }
+ 
+}
--- TestProject1/TestCases.cs
+++ TestProject1/TestCases.cs
@@ -1,95 +1,22 @@
 namespace TestProject1;
 using AventStack.ExtentReports;
-using AventStack.ExtentReports.Reporter;
-using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
+using AventStack.ExtentReports.Gherkin.Model;
+using Microsoft.VisualStudio.CodeCoverage;
 using NUnit.Framework;
 using OpenQA.Selenium;
-using OpenQA.Selenium.Firefox;
-using OpenQA.Selenium.Interactions;
 using OpenQA.Selenium.Support.UI;
 using SeleniumExtras.WaitHelpers;
 using System;
 
+
+
 [TestFixture]
-public class April9th
+public class TestCases : TestBasics
 {
-    private IWebDriver driver;
-    private ExtentReports Reports;
-    private ExtentTest Test;
-    private string reportPath;
-
-    //Ignores ads because fuck em
-    protected void JsClick(IWebElement element)
-    {
-        ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].click();", element);
-    }
-
-    [OneTimeSetUp]
-    public void SingleUse()
-    {
-        //Creates report and determines its location
-        reportPath = Path.Combine(System.Environment.ExpandEnvironmentVariables("%userprofile%/downloads/"), "TestResults.html");
-        var reporter = new ExtentSparkReporter(reportPath);
-        Reports = new ExtentReports();
-        Reports.AttachReporter(reporter);
-    }
-
-    [OneTimeTearDown]
-    public void SingleUseTear()
-    {
-        Reports.Flush();
-        System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
-        {
-            FileName = reportPath,
-            UseShellExecute = true
-        });
-    }
-
-    [SetUp]
-    public void Setup ()
-    {
-        //Runs before EACH test
-        driver = new FirefoxDriver();
-
-        //Creates the object which will keep track of test results
-        Test = Reports.CreateTest(TestContext.CurrentContext.Test.Name);
-    }
-
-    [TearDown]
-    public void Teardown ()
-    {
-        //Retrieves test status from run - if failed takes a screenshot.
-        //If the test passes the status will be updated alone
-        var status = TestContext.CurrentContext.Result.Outcome.Status;
-        var message = TestContext.CurrentContext.Result.Message;
-
-        if (status == NUnit.Framework.Interfaces.TestStatus.Failed)
-        {
-            //Log the actual failure reason fron Nunit before taking the screenshot
-            Test.Log(Status.Fail, $"Assertion failed: {message}");
-
-            var screenshot = ((ITakesScreenshot)driver).GetScreenshot();
-            Test.Fail("Test failed — see screenshot")
-                .AddScreenCaptureFromBase64String(screenshot.AsBase64EncodedString);
-        }
-        else
-        {
-            Test.Pass("Test passed");
-        }
-
-        //Runs after each test
-        //Disposes of previous cache before a new test is run
-        driver?.Quit();
-        driver?.Dispose();
-        driver = null;
-
-    }
-
     //Test 1: Returns the total of women's clothes categories inside the drop down menu
     [Test, Order(1)]
     public void ClothesCategories() 
     {
-        //IWebDriver driver = new FirefoxDriver();
         driver.Navigate().GoToUrl("https://automationexercise.com/");
         Test.Log(Status.Info, "Navigating to automationexercise.com");
 
@@ -101,7 +28,7 @@ public class April9th
         driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
 
         //Get subcategories of women's clothes
-        var subCategories = driver.FindElements(By.XPath("//div[@id='Women']//a"));
+        var subCategories = this.findElementSByXPath("//div[@id='Women']//a");
 
 
         //Log each category name found
@@ -114,7 +41,6 @@ public class April9th
         }
 
         var catString = categoryNames.ToString();
-        //categoryNames.ToString();
         Test.Log(Status.Info, $"Subcategories found under Women's section: {subCategories.Count}, {catString}");
 
 
@@ -134,7 +60,7 @@ public class April9th
     {
         Test.Log(Status.Info, "Navigating to automationexercise.com");
         driver.Navigate().GoToUrl("https://automationexercise.com/");
-        var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
 
         //Validate home page is visible by checking URL
         Assert.That(driver.Url, Is.EqualTo("https://automationexercise.com/"));
@@ -143,7 +69,6 @@ public class April9th
         //Navigates to the 'Products' list
         Test.Log(Status.Info, "Clicking products navigation link");
         IWebElement productList = driver.FindElement(By.XPath("/html/body/header/div/div/div/div[2]/div/ul/li[2]/a"));
-        //productList.Click();
         this.JsClick(productList);
 
         //Waits frot he products page to load, and checks 'All products' title
@@ -155,7 +80,6 @@ public class April9th
         //Selects first item from list
         Test.Log(Status.Info, "Selecting first item from list");
         var productItem = driver.FindElement(By.LinkText("View Product"));
-        //productItem.Click();
         this.JsClick(productItem);
 
         //Wait for product detail page to load
@@ -163,12 +87,13 @@ public class April9th
         Test.Log(Status.Pass, $"Products deatail page loaded, header displayed: {driver.Url}");
 
         //find page elements: product name, category, price, availability, condition and brand.
-        IWebElement productName = driver.FindElement(By.CssSelector(".product-information h2"));
-        IWebElement productCategory = driver.FindElement(By.XPath("//div[@class='product-information']//p[contains(text(), 'Category')]"));
-        IWebElement productPrice = driver.FindElement(By.CssSelector(".product-information span span"));
-        IWebElement availability = driver.FindElement(By.XPath("/html/body/section/div/div/div[2]/div[2]/div[2]/div/p[2]"));
-        IWebElement condition = driver.FindElement(By.XPath("/html/body/section/div/div/div[2]/div[2]/div[2]/div/p[3]"));
-        IWebElement brand = driver.FindElement(By.XPath("/html/body/section/div/div/div[2]/div[2]/div[2]/div/p[4]"));
+        var productName = this.findElementByCSS(".product-information h2");
+        var productCategory = this.findElementByXPath("//div[@class='product-information']//p[contains(text(), 'Category')]");
+        var productPrice = this.findElementByCSS( ".product-information span span");
+        var availability = this.findElementByXPath("/html/body/section/div/div/div[2]/div[2]/div[2]/div/p[2]]");
+        var condition = this.findElementByXPath("/html/body/section/div/div/div[2]/div[2]/div[2]/div/p[2]]");
+        var brand = this.findElementByXPath("/html/body/section/div/div/div[2]/div[2]/div[2]/div/p[3]");
+
 
         // Log all scraped values before asserting so they appear in the report
         // whether the test passes or fails
@@ -204,9 +129,41 @@ public class April9th
         //Testing commit
     }
 
+    //Test case 9: Using the search functionality
     [Test, Order(3)]
-    public void TestMethod3()
+    public void SearchProduct()
     {
+        Test.Log(Status.Info, "Navigating to automationexercise.com");
+        driver.Navigate().GoToUrl("https://automationexercise.com/");
+        var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+
+        Assert.That(driver.Url, Is.EqualTo("https://automationexercise.com/"));
+        Test.Log(Status.Pass, $"Home page confirmed: {driver.Url}");
+
+        Test.Log(Status.Info, "Clicking products navigation link");
+        IWebElement productList = driver.FindElement(By.XPath("/html/body/header/div/div/div/div[2]/div/ul/li[2]/a"));
+        this.JsClick(productList);
+
+        wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//h2[contains(text(),'All Products')]")));
+        IWebElement heading = driver.FindElement(By.XPath("//h2[contains(text(),'All Products')]"));
+        Assert.That(heading.Text, Does.Contain("ALL PRODUCTS"));
+        Test.Log(Status.Pass, $"Products page loaded, header displayed: {heading.Text}");
+
+        //Finding search bar element by Id
+        Test.Log(Status.Info, "Finding search bar element");
+        IWebElement searchBar = driver.FindElement(By.Id("search_product"));
+        
+        //Click search bar and type in product name
+        searchBar.Click();
+        searchBar.SendKeys("Green Side Placket Detail T - Shirt" + Keys.Enter);
+        Test.Log(Status.Info, "Searching for item: Green Side Placket Detail T-Shirt ");
+
+        //Validating Green shirt is visible in search results
+
+        //Clicks on item presented after item search
+        Test.Log(Status.Info, "Selecting green t-shirt item from search results");
+        var productItem = driver.FindElement(By.LinkText("View Product"));
+        this.JsClick(productItem);
 
     }
 
